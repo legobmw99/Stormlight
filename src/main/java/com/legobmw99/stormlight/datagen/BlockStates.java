e0be5669f0e55f538faa40f0b0b455a27289f503
Brian <legobmw99@gmail.com>
2021-06-01 13:20:28 -0400

    More adhesion block - model

--- src/main/java/com/legobmw99/stormlight/datagen/BlockStates.java
+++ src/main/java/com/legobmw99/stormlight/datagen/BlockStates.java
@@ -2,10 +2,14 @@ package com.legobmw99.stormlight.datagen;
 
 import com.legobmw99.stormlight.Stormlight;
 import com.legobmw99.stormlight.modules.world.WorldSetup;
+import com.legobmw99.stormlight.modules.world.block.AdhesionBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.state.properties.AttachFace;
+import net.minecraft.util.Direction;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nonnull;
@@ -17,12 +21,32 @@ public class BlockStates extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        ModelFile layer = models()
-                .withExistingParent("stormlight:adhesion_light", mcLoc("block/snow_height2"))
-                .texture("particle", blockTexture(Blocks.SEA_LANTERN))
-                .texture("texture", blockTexture(Blocks.LIGHT_BLUE_STAINED_GLASS));
+        createAdhesionLayer();
+    }
 
-        simpleBlock(WorldSetup.ADHESION_BLOCK.get(), layer);
+    private void createAdhesionLayer() {
+        ModelFile layer = models().withExistingParent("stormlight:adhesion_light", mcLoc("block/carpet")).texture("wool", blockTexture(Blocks.LIGHT_BLUE_STAINED_GLASS));
+
+        VariantBlockStateBuilder builder = getVariantBuilder(WorldSetup.ADHESION_BLOCK.get());
+
+        for (AttachFace face : AdhesionBlock.FACE.getPossibleValues()) {
+            int xangle = (face == AttachFace.CEILING) ? 180 : (face == AttachFace.WALL) ? 90 : 0;
+            boolean uvlock = face == AttachFace.WALL;
+            for (Direction dir : AdhesionBlock.FACING.getPossibleValues()) {
+                int yangle = (int) dir.toYRot();
+                yangle = face != AttachFace.CEILING ? (yangle + 180) % 360 : yangle;
+                builder
+                        .partialState()
+                        .with(AdhesionBlock.FACE, face)
+                        .with(AdhesionBlock.FACING, dir)
+                        .modelForState()
+                        .modelFile(layer)
+                        .uvLock(uvlock)
+                        .rotationY(yangle)
+                        .rotationX(xangle)
+                        .addModel();
+            }
+        }
     }
 
     @Nonnull
--- src/main/java/com/legobmw99/stormlight/modules/world/block/AdhesionBlock.java
+++ src/main/java/com/legobmw99/stormlight/modules/world/block/AdhesionBlock.java
@@ -3,6 +3,8 @@ package com.legobmw99.stormlight.modules.world.block;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.AttachFace;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -10,8 +12,6 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
@@ -19,29 +19,57 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.Random;
 
-public class AdhesionBlock extends Block {
+public class AdhesionBlock extends HorizontalFaceBlock {
+
+    private static final VoxelShape TOP = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
+    private static final VoxelShape BOTTOM = Block.box(0.0D, 15.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    private static final VoxelShape NORTH = Block.box(0.0D, 0.0D, 15.0D, 16.0D, 16.0D, 16.0D);
+    private static final VoxelShape SOUTH = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 1.0D);
+    private static final VoxelShape EAST = Block.box(0.0D, 0.0D, 0.0D, 1.0D, 16.0D, 16.0D);
+    private static final VoxelShape WEST = Block.box(15.0D, .0D, 0.0D, 16.0D, 16.0D, 16.0D);
+
 
     public AdhesionBlock() {
         super(AbstractBlock.Properties
                       .of(Material.TOP_SNOW)
-                      .lightLevel((d) -> 13)
+                      .lightLevel((d) -> 15)
                       .jumpFactor(0.0F)
                       .noDrops()
                       .strength(-1.0F, 3600000.0F)
                       .randomTicks()
-                      .speedFactor(-0.00001F)
+                      .speedFactor(0.0F)
                       .noOcclusion()
                       .isValidSpawn((a, b, c, d) -> false)
                       .isRedstoneConductor((a, b, c) -> false)
                       .isSuffocating((a, b, c) -> false)
                       .isViewBlocking((a, b, c) -> false)
                       .sound(SoundType.GLASS));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(FACE, AttachFace.WALL));
+
     }
 
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext ctx) {
-        return Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+        switch (state.getValue(FACE)) {
+            case FLOOR:
+                return TOP;
+            case CEILING:
+                return BOTTOM;
+            case WALL:
+            default:
+                switch (state.getValue(FACING)) {
+                    case EAST:
+                        return EAST;
+                    case WEST:
+                        return WEST;
+                    case SOUTH:
+                        return SOUTH;
+                    case NORTH:
+                    default:
+                        return NORTH;
+                }
+        }
     }
 
     @Override
@@ -71,17 +99,8 @@ public class AdhesionBlock extends Block {
     }
 
     @Override
-    public boolean canSurvive(BlockState state, IWorldReader world, BlockPos pos) {
-        BlockState blockstate = world.getBlockState(pos.below());
-        if (!blockstate.is(Blocks.BARRIER)) {
-            return Block.isFaceFull(blockstate.getCollisionShape(world, pos.below()), Direction.UP);
-        }
-        return false;
-    }
-
-    @Override
-    public BlockState updateShape(BlockState state, Direction direction, BlockState state2, IWorld world, BlockPos pos1, BlockPos pos2) {
-        return !state.canSurvive(world, pos1) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, direction, state2, world, pos1, pos2);
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING, FACE);
     }
 
 
@@ -95,7 +114,34 @@ public class AdhesionBlock extends Block {
     @Override
     public void entityInside(BlockState state, World world, BlockPos pos, Entity entity) {
         if (!entity.isNoGravity()) {
-            entity.setDeltaMovement(Vector3d.atBottomCenterOf(pos).subtract(entity.position()).normalize());
+            Vector3d target;
+
+            switch (state.getValue(FACE)) {
+                case FLOOR:
+                    target = Vector3d.atBottomCenterOf(pos);
+                    break;
+                case CEILING:
+                    target = Vector3d.atBottomCenterOf(pos.above());
+                    break;
+                case WALL:
+                default:
+                    switch (state.getValue(FACING)) {
+                        case EAST:
+                            target = Vector3d.atCenterOf(pos.below().west());
+                            break;
+                        case WEST:
+                            target = Vector3d.atCenterOf(pos.below().east());
+                            break;
+                        case SOUTH:
+                            target = Vector3d.atCenterOf(pos.below().north());
+                            break;
+                        case NORTH:
+                        default:
+                            target = Vector3d.atCenterOf(pos.below().south());
+                            break;
+                    }
+            }
+            entity.setDeltaMovement(target.subtract(entity.position()).normalize());
             entity.makeStuckInBlock(state, new Vector3d(0.01, 1, 0.01));
         }
     }
